3552b74f0567042fdf7eb972237e61465235d189
E-Gregorio <elyer.arg.67@gmail.com>
2025-02-15 12:31:55 -0300

    feat: Configuracion archivo de prueba test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,17 @@
+# tests/test_api.py
+
+def test_sum():
+    """Prueba simple de suma"""
+    assert 1 + 1 == 2
+
+def test_subtract():
+    """Prueba simple de resta"""
+    assert 5 - 3 == 2
+
+def test_multiply():
+    """Prueba simple de multiplicación"""
+    assert 2 * 3 == 6
+
+def test_divide():
+    """Prueba simple de división"""
+    assert 6 / 2 == 3
